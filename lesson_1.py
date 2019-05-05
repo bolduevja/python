8af0335a17427655d47b05b31ee134dca787d47f
toxinrap <toxinrao@me.com>
2019-05-05 08:57:32 +0200

    решил задания из ветки normal и исправил небольшие ошибки

--- lesson_1.py
+++ lesson_1.py
@@ -1,18 +1,32 @@
-# task1
+# easyTask1
 
 # a = int(input('введите первое число '))
-# b = int(input("введите второе число "))
+# b = int(input('введите второе число '))
 # print('первое число ' + str(int(a)) + ' второе число ' + str(int(b)) + ' а их произведение ' + str(a * b))
 
-# task2
+# easyTask2
 
 # c = int(input('введите число '))
 # print('результат ' + str(int(c + 2)))
 
-# task3
+# easyTask3
 
 # age = int(input('введите ваш возраст '))
 # if age >= 18:
 #     print('Доступ разрешен')
 # else:
 #     print('Извините, пользование данным ресурсом только с 18 лет')
+
+# normalTask1
+
+# number = int(input('введите число от 0 до 10 '))
+# while number <= 0 or number > 10:
+#     number = int(input('Число не верное! введите число от 0 до 10 '))
+# print(number ** 2)
+
+# normalTask2
+
+# f = (input('введите первое число '))
+# g = (input("введите второе число "))
+# f, g = g, f
+# print('значение 1 ' + f + ' значение 2 ' + g)
